fa2fcc9814877c6dbc83e3b0c8e20bf7317c7e51
jgmv <53367553+jgmv@users.noreply.github.com>
2021-03-24 14:10:47 +0100

    Update specificity_assays_bp.R

--- R/specificity_assays_bp.R
+++ R/specificity_assays_bp.R
@@ -1,7 +1,7 @@
 #' Plots Boxplots of Specificity Assays Next to a Host Plant Phylogeny
 #'
 #' Plots boxplots of fungus-inoculated vs uninoculated plants, next to a phylogeny of the host plant
-#' @param x data frame with the input data. One column named 'species' must contain the plant abbreviated names.
+#' @param x data frame with the input data. One column named 'species' must contain the plant abbreviated names, and a column 'fungus' must contain the fungal treatment.
 #' @param var name of column in 'x' where the variable to be plotted is stored.
 #' @param x_lab optional label for the x axis.   
 #' @return A stacked plot with a phylogeny and a boxplot.
@@ -10,7 +10,12 @@ specificity_assays_bp <- function(x, var = "weight", x_lab = "var") {
   require(patchwork)
   x$var <- x[, var]
   tree <- phy_plants(sel = unique(x$species))
-  tr <- ggtree(tree)
+
+  scale_y_tree <- function(expand = expansion(0, 0.6), ...){
+    scale_y_continuous(expand = expand, ...)
+  }
+  tr <- ggtree(tree) + scale_y_tree()
+
   p <- x %>%
     dplyr::mutate(species = forcats::fct_relevel(species, tree$tip.label)) %>%
     ggplot(aes(y = species, x = var, fill = fungus)) + 
@@ -24,5 +29,6 @@ specificity_assays_bp <- function(x, var = "weight", x_lab = "var") {
     legend.text = element_text(size = 12)) +
     geom_point(aes(color = fungus), position = position_jitterdodge()) +
     xlab(x_lab) + ylab("")
+
   tr + p #+ plot_annotation(tag_levels = "A")
 }
