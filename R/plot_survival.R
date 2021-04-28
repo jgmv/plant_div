d43c61e5e595f239026c48b79bb8adc21f9f21a5
jgmv <53367553+jgmv@users.noreply.github.com>
2021-04-28 15:41:52 +0200

    Add files via upload

---
+++ R/plot_survival.R
@@ -0,0 +1,17 @@
+#' Plots Probability of Survival over Time
+#'
+#' Plots lines showing the probability of survival over time
+#' @param data data frame with the input data.
+#' @param plant abbreviated name of the plant species to be plotted.
+#' @return A plot with probability of survival over time.
+#' @export
+plot_survival <- function(data, plant) {
+  require(ggplot2)
+  inoc_date <- min(as.Date(data$date))
+  data$dpi <- abs(inoc_date - as.Date(data$date))
+  x <- droplevels(data[data$species == plant, ])
+  x[x$symptoms > 0, "symptoms"] <- 1
+  fit <- survival::survfit(Surv(dpi, symptoms) ~ fungus, data = x)
+  survminer::ggsurvplot(fit, data = x, conf.int = T, main = plant,
+    legend = "right", legend.labs = unique(x$fungus))
+}
